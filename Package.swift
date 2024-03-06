a360b01db8101e1d3d6c1db5b8b59fcf3189b6bc
Aunish <aunishkewat28@gmail.com>
2024-03-06 13:54:40 +0530

    update framework

--- Package.swift
+++ Package.swift
@@ -6,18 +6,14 @@ import PackageDescription
 let package = Package(
     name: "CustomNetworkPackage",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CustomNetworkPackage",
-            targets: ["CustomNetworkPackage"]),
+            name: "CustomNetwork",
+            targets: ["CustomNetwork"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CustomNetworkPackage"),
-        .testTarget(
-            name: "CustomNetworkPackageTests",
-            dependencies: ["CustomNetworkPackage"]),
+        .binaryTarget(
+            name: "CustomNetwork",
+            path: "./CustomNetwork.xcframework"
+        ),
     ]
 )
